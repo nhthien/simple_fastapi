39f1dbf0f1df620d044da0f8e7b18fc11b6b68d4
thien.nh <thien.nh@namitech.io>
2024-07-10 15:43:56 +0700

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World Thien"}
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, port=4000, host='0.0.0.0')
