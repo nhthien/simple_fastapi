456ba769e1f7245553edafc204eab71526722841
Thien Nguyen <nhthien8x@gmail.com>
2024-07-11 10:32:50 +0000

    auto pull image

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World Thien haha", "url": "root", "test": "test k8s replication control"}
+    return {"message": "Hello World Thien haha", "url": "root", "test": "test k8s deployment with setting imagePullPolicy: Always"}
 
 
 if __name__ == '__main__':
