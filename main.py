7c2e3f3bcdfae53262cfe98e289c246a391b3cf9
Thien Nguyen <nhthien8x@gmail.com>
2024-07-11 08:47:11 +0000

    test replication control

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World Thien haha", "url": "root", "test": "test k8s"}
+    return {"message": "Hello World Thien haha", "url": "root", "test": "test k8s replication control"}
 
 
 if __name__ == '__main__':
